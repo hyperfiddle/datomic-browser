c018c2c0cc84c5ad23f8fe1da639ba83dbfe8f98
Geoffrey Gaillard <geoffrey.gaillard@hey.com>
2025-06-25 12:01:49 +0200

    [hf-starter-app] inform user another datomic instance is already running

--- run_datomic.sh
+++ run_datomic.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+set -e
+nc -z localhost 4334 2>/dev/null && { echo "Port 4334 already in use"; exit 1; } || true
+
 set -eux -o pipefail
 
 ./state/datomic-pro/bin/transactor config/samples/dev-transactor-template.properties >>state/datomic.log 2>&1 &
