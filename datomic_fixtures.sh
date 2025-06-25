5a7190217e2d168f1ca8aa3ac6e5c1c9fada4b6d
Geoffrey Gaillard <geoffrey.gaillard@hey.com>
2025-06-25 11:58:25 +0200

    [hf-starter-app] fixtures: prevent datomic port crash

--- datomic_fixtures.sh
+++ datomic_fixtures.sh
@@ -16,14 +16,18 @@ if [ ! -d "mbrainz" ]; then
     tar -xf mbrainz.tar
 fi
 
-datomic-pro/bin/transactor config/samples/dev-transactor-template.properties >>datomic.log 2>&1 &
+# Pick a random port for a short-lived datomic instance
+while PORT=$((RANDOM % 60000 + 5000)); lsof -i:$PORT >/dev/null 2>&1; do :; done
+sed "s/^port=.*/port=$PORT/" datomic-pro/config/samples/dev-transactor-template.properties > datomic-pro/config/fixtures-transactor.properties
+
+datomic-pro/bin/transactor config/fixtures-transactor.properties &
 datomic_transactor_pid=$!
 
 set +ex
-echo "waiting for Datomic port..."
-while ! timeout 1 bash -c "echo > /dev/tcp/localhost/4334 2> /dev/null" 2> /dev/null; do sleep 1; done
+echo "waiting for Datomic to start on port $PORT..."
+while ! timeout 1 bash -c "echo > /dev/tcp/localhost/$PORT 2> /dev/null" 2> /dev/null; do sleep 1; done
 set -ex
 
 # https://datomic.narkive.com/OUskfRdr/backup-error
-datomic-pro/bin/datomic restore-db file:`pwd`/mbrainz-1968-1973 datomic:dev://localhost:4334/mbrainz-1968-1973
+datomic-pro/bin/datomic restore-db file:`pwd`/mbrainz-1968-1973 datomic:dev://localhost:$PORT/mbrainz-1968-1973
 kill $datomic_transactor_pid
