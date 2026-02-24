495d25528e2f282ddafab0ff836e96b0c34e92bb
Dustin Getz <dustin.getz@gmail.com>
2026-02-24 09:55:05 -0500

    www.clojure.net

--- src-dev/user.clj
+++ src-dev/user.clj
@@ -9,13 +9,38 @@
     '[datomic.api :as d]
     '[dustingetz.nav-datomic :as nav]
     '[hyperfiddle.navigator-agent :as agent])
+
+  (require '[clojure.repl.deps :refer [add-libs]])
+  (add-libs '{com.hyperfiddle/hyperfiddle-agent {:mvn/version "v0-alpha-SNAPSHOT"}})
+
+  (require '[hyperfiddle.navigator-agent :as agent] '[hyperfiddle.hfql2 :refer [hfql]])
+
   (d/get-database-names "datomic:dev://localhost:4334/*")
-  (def proxy-uri "wss://index.clojure.net/agent")
+  (def uri "datomic:dev://localhost:4334/mbrainz-1968-1973")
+  (def test-db (d/db (d/connect uri)))
+
+  (def ^:dynamic *db*)
+
+  (defn schema []
+    (d/q '[:find [(pull ?e [:db/ident
+                            {:db/valueType [:db/ident]}
+                            {:db/cardinality [:db/ident]}
+                            *]) ...]
+           :where [?e :db/valueType _]] *db*))
+
+  (def sitemap {`schema (hfql {(schema) {* [:db/ident
+                                            {:db/valueType {:db/ident name}}
+                                            {:db/cardinality {:db/ident name}}]}})})
+
+  (def agent (agent/connect! "wss://index.clojure.net/agent" sitemap
+               (fn [] {#'*db* test-db})))
+
+
 
   ;; Single-database — connect to specific db
-  (def my-agent (agent/connect! proxy-uri nav/rich-sitemap (nav/make-setup-fn "datomic:dev://localhost:4334/mbrainz-full")))
+  (def agent (agent/connect! proxy-uri nav/sitemap (nav/make-setup-fn "datomic:dev://localhost:4334/mbrainz-full")))
 
   ;; Dynamic — multi-database with Inject route segment
-  (def my-agent (agent/connect! proxy-uri nav/rich-sitemap (nav/make-setup-fn "datomic:dev://localhost:4334/*")))
+  (def agent (agent/connect! proxy-uri nav/sitemap (nav/make-setup-fn "datomic:dev://localhost:4334/*")))
 
-  (agent/disconnect! my-agent))
+  (agent/disconnect! agent))
